533d80a3b75554583f447b9724c2a09faa81643b
Tanyapavliuk <122897433+Tanyapavliuk@users.noreply.github.com>
2023-06-01 11:38:27 +0300

    add simplelightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const refs = {
+    wrapper: document.querySelector(".gallery"),
+}
 
-console.log(galleryItems);
+const gellaryItemsEl = galleryItems.map(({preview, original, description}) => {
+    return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a></li>`}).join("");
+
+refs.wrapper.insertAdjacentHTML("beforeend",gellaryItemsEl);
+
+refs.wrapper.addEventListener("click", onClickGellaryItem);
+
+function onClickGellaryItem(evt) {
+    evt.preventDefault();
+    if (evt.target.nodeName !== "IMG") {
+        return;
+    }
+}
